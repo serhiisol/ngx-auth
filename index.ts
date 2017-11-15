b3d7b118b78c2eda8ca1d3644d01c7bc255a432c
Serhii Solonko <sergeysolonko@gmail.com>
2017-11-15 13:22:25 +0100

    feat: angular 5 and latest cli support

--- index.ts
+++
@@ -1,5 +0,0 @@
-export { AuthService } from './src';
-export { PublicGuard } from './src';
-export { ProtectedGuard } from './src';
-export { AUTH_SERVICE, PUBLIC_FALLBACK_PAGE_URI, PROTECTED_FALLBACK_PAGE_URI } from './src';
-export { AuthModule } from './src';
